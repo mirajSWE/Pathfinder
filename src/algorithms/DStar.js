2fd9b9d50c8dd3ffa102dca648847dd5cc667a3d
Nate Boldt <nate.h.boldt@gmail.com>
2021-03-10 18:06:48 -0600

    DStar Node Wrapper

--- src/algorithms/DStar.js
+++ src/algorithms/DStar.js
@@ -11,19 +11,47 @@ class DStar {
         this.openList = [];
     }
 
-    runAlgorithm() {
-        this.openList.push(this.startNode);
-
-        console.log(this.openList);
-
-        this.updateDStarNode(this.graph[2][2], 4);
-
+    dStarNode(node, dStatus, dCost, backTrackPointer) {
+        const dStarNode = {
+            generalNode: node,
+            dStatus: dStatus,
+            dCost: dCost,
+            backTrackPointer: backTrackPointer,
+        }
+        return dStarNode;
     }
 
-    updateDStarNode(node, newStatus) {
+    // DStar node wrapper to hold d star statuses, backpointer to target, and
+
+    runAlgorithm() {
+        this.openList.push(this.endNode);
+
+        // console.log(this.openList);
+        //
+        this.updateGeneralNode(this.graph[2][2], 4);
+
+        // while (this.openList.length > 0) {
+        //     // do
+        // }
+    }
+
+
+
+    updateGeneralNode(node, newStatus) {
         node.status = newStatus;
     }
 
+    updateDStarNodeStatus(dStarNode, newDStatus) {
+        dStarNode.dStatus = newDStatus;
+    }
+
+    updateDStarNodeBackTracker(dStarNode, newBackTracker) {
+        dStarNode.backTrackPointer = newBackTracker;
+    }
+
+    updateDStarNodeCost(dStarNode, newDCost) {
+        dStarNode.dCost = newDCost;
+    }
 }
 
 export default DStar;
